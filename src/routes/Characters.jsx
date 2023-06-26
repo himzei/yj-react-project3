d2aceef62ea1f6928329c74e43581f6998d545ea
himzei <himzei@gmail.com>
2023-06-26 16:28:16 +0900

    0626

--- src/routes/Characters.jsx
+++ src/routes/Characters.jsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import SkeletonList from "../components/SkeletonList";
 import Pagination from "react-js-pagination";
 import "./Paging.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function Characters() {
   const [numLimit, setNumLimit] = useState(6);
@@ -30,108 +31,112 @@ export default function Characters() {
   const handlePageChange = (page) => {
     setPage(page);
   };
-  console.log(data);
 
   return (
-    <VStack w="full" pb="16">
-      <Box w="full" h="64" overflow="hidden">
-        <Image
-          w="full"
-          h="full"
-          objectFit="cover"
-          src="https://images.unsplash.com/photo-1562563436-a73bab993173?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="Characters Img"
-        />
-      </Box>
-      <VStack w="7xl">
-        {/* 타이틀 */}
-        <HStack w="full" py="16" justifyContent="space-between">
-          <Box>
-            <Text fontSize={32} fontWeight="bold" textTransform="uppercase">
-              Characters
-            </Text>
-          </Box>
-          <Select w="32" onChange={handleChange}>
-            <option value="6">6</option>
-            <option value="12">12</option>
-            <option value="18">18</option>
-            <option value="24">24</option>
-            <option value="30">30</option>
-            <option value="36">36</option>
-          </Select>
-        </HStack>
-        {/* 게시판 */}
-        <Grid templateColumns={"repeat(6, 1fr)"} w="full" gap="4">
-          {isLoading && <SkeletonList />}
-          {data?.data?.results.map((item, i) => (
-            <GridItem w="full" bg="red.500" role="group">
-              <VStack w="full">
-                {/* 1번째 박스 (이미지) */}
-                <Box w="full" h="48" overflow="hidden">
-                  <Image
-                    transition="0.4s"
-                    _groupHover={{
-                      transform: "scale(1.2)",
-                    }}
-                    w="full"
-                    h="full"
-                    objectFit="cover"
-                    src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                    alt="characters content"
-                  />
-                </Box>
-                {/* 2번째 박스 (타이틀) */}
-                <Box w="full" h="36" position="relative" overflow="hidden">
-                  {/* 호버시 이동하는 빈박스 */}
-                  <Box
-                    transition="0.4s"
-                    position="absolute"
-                    top="0"
-                    left="0"
-                    w="full"
-                    h="full"
-                    bg="gray.800"
-                    _groupHover={{
-                      top: "180px",
-                    }}
-                  />
-                  {/* 오른쪽 밑부분 자르기 위한 빈박스 */}
-                  <Box
-                    position="absolute"
-                    bottom="-15px"
-                    right="-15px"
-                    bg="white"
-                    w="30px"
-                    h="30px"
-                    transform={"rotate(45deg) scale(2)"}
-                  />
-                  {/* 타이틀을 넣기 위한 부분 */}
-                  <Text
-                    position="absolute"
-                    top="10px"
-                    left="10px"
-                    color="white"
-                    fontSize="16"
-                    fontWeight="semibold"
-                  >
-                    {item.name}
-                  </Text>
-                </Box>
-              </VStack>
-            </GridItem>
-          ))}
-        </Grid>
-        {/* 페이지네이션 */}
-        <Box>
-          <Pagination
-            activePage={page}
-            itemsCountPerPage={numLimit}
-            totalItemsCount={total}
-            pageRangeDisplayed={5}
-            onChange={handlePageChange}
+    <HelmetProvider>
+      <Helmet>
+        <title>마블페이지-Characters</title>
+      </Helmet>
+      <VStack w="full" pb="16">
+        <Box w="full" h="64" overflow="hidden">
+          <Image
+            w="full"
+            h="full"
+            objectFit="cover"
+            src="https://images.unsplash.com/photo-1562563436-a73bab993173?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Characters Img"
           />
         </Box>
+        <VStack w="7xl">
+          {/* 타이틀 */}
+          <HStack w="full" py="16" justifyContent="space-between">
+            <Box>
+              <Text fontSize={32} fontWeight="bold" textTransform="uppercase">
+                Characters
+              </Text>
+            </Box>
+            <Select w="32" onChange={handleChange}>
+              <option value="6">6</option>
+              <option value="12">12</option>
+              <option value="18">18</option>
+              <option value="24">24</option>
+              <option value="30">30</option>
+              <option value="36">36</option>
+            </Select>
+          </HStack>
+          {/* 게시판 */}
+          <Grid templateColumns={"repeat(6, 1fr)"} w="full" gap="4">
+            {isLoading && <SkeletonList />}
+            {data?.data?.results.map((item, i) => (
+              <GridItem w="full" bg="red.500" role="group">
+                <VStack w="full">
+                  {/* 1번째 박스 (이미지) */}
+                  <Box w="full" h="48" overflow="hidden">
+                    <Image
+                      transition="0.4s"
+                      _groupHover={{
+                        transform: "scale(1.2)",
+                      }}
+                      w="full"
+                      h="full"
+                      objectFit="cover"
+                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                      alt="characters content"
+                    />
+                  </Box>
+                  {/* 2번째 박스 (타이틀) */}
+                  <Box w="full" h="36" position="relative" overflow="hidden">
+                    {/* 호버시 이동하는 빈박스 */}
+                    <Box
+                      transition="0.4s"
+                      position="absolute"
+                      top="0"
+                      left="0"
+                      w="full"
+                      h="full"
+                      bg="gray.800"
+                      _groupHover={{
+                        top: "180px",
+                      }}
+                    />
+                    {/* 오른쪽 밑부분 자르기 위한 빈박스 */}
+                    <Box
+                      position="absolute"
+                      bottom="-15px"
+                      right="-15px"
+                      bg="white"
+                      w="30px"
+                      h="30px"
+                      transform={"rotate(45deg) scale(2)"}
+                    />
+                    {/* 타이틀을 넣기 위한 부분 */}
+                    <Text
+                      position="absolute"
+                      top="10px"
+                      left="10px"
+                      color="white"
+                      fontSize="16"
+                      fontWeight="semibold"
+                    >
+                      {item.name}
+                    </Text>
+                  </Box>
+                </VStack>
+              </GridItem>
+            ))}
+          </Grid>
+          {/* 페이지네이션 */}
+          <Box>
+            <Pagination
+              activePage={page}
+              itemsCountPerPage={numLimit}
+              totalItemsCount={total}
+              pageRangeDisplayed={5}
+              onChange={handlePageChange}
+            />
+          </Box>
+        </VStack>
       </VStack>
-    </VStack>
+    </HelmetProvider>
   );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,6 +16,10 @@ import Slider from "react-slick";
 import { Link } from "react-router-dom";
 import { comicsList, eventsList } from "../api";
 import SkeletonList from "../components/SkeletonList";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const settings = {
   dots: false,
@@ -46,6 +50,9 @@ const featuresLists = [
 ];
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   const { data, isLoading } = useQuery("repoData", comicsList);
   console.log("로딩 : ", isLoading, "코믹데이터 : ", data);
   const { data: eventsData, isLoading: eventsIsLoading } = useQuery(
@@ -55,111 +62,136 @@ export default function Home() {
 
   return (
     <>
-      {/* 캐러셀 */}
-      <Box>
-        <CarouselSlick />
-      </Box>
-
-      {/* 특장점 */}
-      <HStack w="full" justifyContent="center" py="16" bg="gray.100">
-        <Grid gap="4" w="7xl" templateColumns={"repeat(3, 1fr)"}>
-          {featuresLists.map((item, i) => (
-            <CardItems key={i} item={item} />
-          ))}
-        </Grid>
-      </HStack>
-
-      {/* 기울어진 이미지 타이틀 */}
-      <TitleImageSkew
-        title="Comics"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est quos in cum delectus numquam corrupti eligendi unde itaque, natus voluptatem, esse corporis voluptate perferendis adipisci molestiae. Ipsa, non ducimus?"
-        imgUrl="https://assets.vogue.in/photos/5ce412599cc0c0b8f5f9b4bf/4:3/w_1440,h_1080,c_limit/Everything-you-need-to-know-before-watching-Marvel-movies-this-year.jpg"
-      />
-
-      {/* Comics 컨텐츠 리스트 */}
-      <VStack w="full" position="relative" h="400px">
-        {/* 힌박스 위로 올라오게 하는 범위지정 */}
-        <Box position="absolute" w="7xl" py="8" px="2" top={-16} bg="white">
-          {isLoading ? <SkeletonList /> : ""}
-          <Slider {...settings}>
-            {data?.data?.results?.map((item, i) => (
-              <Link to={`/comics/${item.id}?type=comics`}>
-                <VStack key={i} w="full" h="full" role="group" cursor="pointer">
-                  <Box
-                    overflow="hidden"
-                    w="170px"
-                    h="240px"
-                    _groupHover={{ transform: "scale(1.1)" }}
-                    transition={"0.4s"}
-                  >
-                    <Image
-                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                      alt={`Comics ${i}`}
-                      w="full"
-                      h="full"
-                      objectFit="cover"
-                    />
-                  </Box>
-                  <Text
-                    transition={"0.4s"}
-                    _groupHover={{ color: "red.500", fontWeight: "600" }}
-                    mt="2"
-                    px="2"
-                  >
-                    {item.title.substr(0, 36)}
-                  </Text>
-                </VStack>
-              </Link>
-            ))}
-          </Slider>
+      <HelmetProvider>
+        <Helmet>
+          <title>마블 홈페이지 입니다.</title>
+        </Helmet>
+        {/* 캐러셀 */}
+        <Box>
+          <CarouselSlick />
         </Box>
-      </VStack>
 
-      {/* 이벤트 타이틀 */}
-      {/* 기울어진 이미지 타이틀 */}
-      <TitleImageSkew
-        title="Events"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est quos in cum delectus numquam corrupti eligendi unde itaque, natus voluptatem, esse corporis voluptate perferendis adipisci molestiae. Ipsa, non ducimus?"
-        imgUrl="https://terrigen-cdn-dev.marvel.com/content/prod/1x/sre7000_trl_comp_wta_v0265.1061_r_0.jpg"
-      />
-
-      {/* Events 컨텐츠 리스트 */}
-      <VStack w="full" position="relative" h="400px">
-        {/* 힌박스 위로 올라오게 하는 범위지정 */}
-        <Box position="absolute" w="7xl" py="8" px="2" top={-16} bg="white">
-          <Slider {...settings}>
-            {eventsData?.data?.results?.map((item, i) => (
-              <Link to={`/events/${item.id}?type=events`}>
-                <VStack key={i} w="full" h="full" role="group" cursor="pointer">
-                  <Box
-                    overflow="hidden"
-                    w="170px"
-                    h="240px"
-                    _groupHover={{ transform: "scale(1.1)" }}
-                    transition={"0.4s"}
-                  >
-                    <Image
-                      src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
-                      alt={`Comics ${i}`}
-                      w="full"
-                      h="full"
-                      objectFit="cover"
-                    />
-                  </Box>
-                  <Text
-                    transition={"0.4s"}
-                    _groupHover={{ color: "red.500", fontWeight: "600" }}
-                    mt="2"
-                    px="2"
-                  >
-                    {item.title.substr(0, 36)}
-                  </Text>
-                </VStack>
-              </Link>
+        {/* 특장점 */}
+        <HStack
+          w="full"
+          justifyContent="center"
+          py="16"
+          bg="gray.100"
+          data-aos="fade-up"
+        >
+          <Grid gap="4" w="7xl" templateColumns={"repeat(3, 1fr)"}>
+            {featuresLists.map((item, i) => (
+              <CardItems key={i} item={item} />
             ))}
-          </Slider>
-        </Box>
-      </VStack>
+          </Grid>
+        </HStack>
+
+        {/* 기울어진 이미지 타이틀 */}
+        <div data-aos="fade-up">
+          <TitleImageSkew
+            title="Comics"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est quos in cum delectus numquam corrupti eligendi unde itaque, natus voluptatem, esse corporis voluptate perferendis adipisci molestiae. Ipsa, non ducimus?"
+            imgUrl="https://assets.vogue.in/photos/5ce412599cc0c0b8f5f9b4bf/4:3/w_1440,h_1080,c_limit/Everything-you-need-to-know-before-watching-Marvel-movies-this-year.jpg"
+          />
+        </div>
+
+        {/* Comics 컨텐츠 리스트 */}
+        <VStack data-aos="fade-up" w="full" position="relative" h="400px">
+          {/* 힌박스 위로 올라오게 하는 범위지정 */}
+          <Box position="absolute" w="7xl" py="8" px="2" top={-16} bg="white">
+            {isLoading ? <SkeletonList /> : ""}
+            <Slider {...settings}>
+              {data?.data?.results?.map((item, i) => (
+                <Link to={`/comics/${item.id}?type=comics`}>
+                  <VStack
+                    key={i}
+                    w="full"
+                    h="full"
+                    role="group"
+                    cursor="pointer"
+                  >
+                    <Box
+                      overflow="hidden"
+                      w="170px"
+                      h="240px"
+                      _groupHover={{ transform: "scale(1.1)" }}
+                      transition={"0.4s"}
+                    >
+                      <Image
+                        src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                        alt={`Comics ${i}`}
+                        w="full"
+                        h="full"
+                        objectFit="cover"
+                      />
+                    </Box>
+                    <Text
+                      transition={"0.4s"}
+                      _groupHover={{ color: "red.500", fontWeight: "600" }}
+                      mt="2"
+                      px="2"
+                    >
+                      {item.title.substr(0, 36)}
+                    </Text>
+                  </VStack>
+                </Link>
+              ))}
+            </Slider>
+          </Box>
+        </VStack>
+
+        {/* 이벤트 타이틀 */}
+        {/* 기울어진 이미지 타이틀 */}
+        <TitleImageSkew
+          title="Events"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt est quos in cum delectus numquam corrupti eligendi unde itaque, natus voluptatem, esse corporis voluptate perferendis adipisci molestiae. Ipsa, non ducimus?"
+          imgUrl="https://terrigen-cdn-dev.marvel.com/content/prod/1x/sre7000_trl_comp_wta_v0265.1061_r_0.jpg"
+        />
+
+        {/* Events 컨텐츠 리스트 */}
+        <VStack w="full" position="relative" h="400px">
+          {/* 힌박스 위로 올라오게 하는 범위지정 */}
+          <Box position="absolute" w="7xl" py="8" px="2" top={-16} bg="white">
+            <Slider {...settings}>
+              {eventsData?.data?.results?.map((item, i) => (
+                <Link to={`/events/${item.id}?type=events`}>
+                  <VStack
+                    key={i}
+                    w="full"
+                    h="full"
+                    role="group"
+                    cursor="pointer"
+                  >
+                    <Box
+                      overflow="hidden"
+                      w="170px"
+                      h="240px"
+                      _groupHover={{ transform: "scale(1.1)" }}
+                      transition={"0.4s"}
+                    >
+                      <Image
+                        src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                        alt={`Comics ${i}`}
+                        w="full"
+                        h="full"
+                        objectFit="cover"
+                      />
+                    </Box>
+                    <Text
+                      transition={"0.4s"}
+                      _groupHover={{ color: "red.500", fontWeight: "600" }}
+                      mt="2"
+                      px="2"
+                    >
+                      {item.title.substr(0, 36)}
+                    </Text>
+                  </VStack>
+                </Link>
+              ))}
+            </Slider>
+          </Box>
+        </VStack>
+      </HelmetProvider>
     </>
   );
 }
