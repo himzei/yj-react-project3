a885f02779dcfd954838b3b0aa2fd7e16a70df8b
himzei <himzei@gmail.com>
2023-06-26 11:59:30 +0900

    0626 12:00

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const BASE_PATH = "https://gateway.marvel.com:443";
+const API_KEY = process.env.REACT_APP_PUBLIC_KEY;
+
+export async function comicsList() {
+  return await fetch(
+    `${BASE_PATH}/v1/public/comics?format=comic&apikey=${API_KEY}`
+  ).then((res) => res.json());
+}
+
+export async function eventsList() {
+  return await fetch(`${BASE_PATH}/v1/public/events?apikey=${API_KEY}`).then(
+    (res) => res.json()
+  );
+}
--- src/routes/Characters.jsx
+++ src/routes/Characters.jsx
@@ -1,5 +1,48 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  HStack,
+  Image,
+  Select,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-export default function Characters(){
-    return <Box py="16">Characters</Box>
+export default function Characters() {
+  return (
+    <VStack w="full">
+      <Box w="full" h="64" overflow="hidden">
+        <Image
+          w="full"
+          h="full"
+          objectFit="cover"
+          src="https://images.unsplash.com/photo-1562563436-a73bab993173?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt="Characters Img"
+        />
+      </Box>
+      <VStack w="7xl">
+        {/* 타이틀 */}
+        <HStack w="full" py="16" justifyContent="space-between">
+          <Box>
+            <Text fontSize={32} fontWeight="bold" textTransform="uppercase">
+              Characters
+            </Text>
+          </Box>
+          <Select w="32">
+            <option value="6">6</option>
+            <option value="12">12</option>
+            <option value="18">18</option>
+            <option value="24">24</option>
+            <option value="30">30</option>
+            <option value="36">36</option>
+          </Select>
+        </HStack>
+        {/* 게시판 */}
+        <Grid templateColumns={"reapeat(6, 1fr)"} w="full" gap="4">
+          <GridItem w="full" bg="red.500"></GridItem>
+        </Grid>
+      </VStack>
+    </VStack>
+  );
 }
